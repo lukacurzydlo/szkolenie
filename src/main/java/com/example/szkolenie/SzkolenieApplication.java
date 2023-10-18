7e7ffab3a7bddcdc99d3c0fa6c681016cc63a503
lukacurzydlo <curzydlolukasz3@gmail.com>
2023-10-18 13:58:49 +0200

    initial-commit

--- src/main/java/com/example/szkolenie/SzkolenieApplication.java
+++ src/main/java/com/example/szkolenie/SzkolenieApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SzkolenieApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SzkolenieApplication.class, args);
 	}
