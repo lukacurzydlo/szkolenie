c3804937071c54651e0490a38580de23a038f52d
lukacurzydlo <curzydlolukasz3@gmail.com>
2023-10-19 19:17:22 +0200

    initial-commit

--- src/test/java/com/example/szkolenie/SzkolenieApplicationTests.java
+++ src/test/java/com/example/szkolenie/SzkolenieApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.szkolenie;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SzkolenieApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.szkolenie;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SzkolenieApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
